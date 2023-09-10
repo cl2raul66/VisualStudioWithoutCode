7e3c7938354e750307fb6cfb8d5ec6c1186aa3ca
Raúl Avila Catalá <cl2raul66@hotmail.com>
2023-09-10 03:17:55 -0600

    Delete src/FlowChartEditor directory

--- src/FlowChartEditor/Models/Flow.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FlowChartEditor.Models;
-
-public class Flow
-{
-    public int Idx { get; set; }
-}
--- src/FlowChartEditor/Tools/Enumerations/Instructions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FlowChartEditor.Tools.Enumerations;
-
-public enum Instructions
-{
-    Begin,End, In, Out, Connector, Decision_if, Decision_switch, Loop_do, Loop_while, Loop_for, Loop_foreach, Process
-}
